288a27be07275a16599ccd21989f155254bf1553
Lukas Kundelis <lukas.kundelis@gmail.com>
2023-12-14 14:16:43 +0000

    Login activity add

--- app/src/main/java/com/PsykoloGG/foodieswans/LoginActivity.kt
+++ app/src/main/java/com/PsykoloGG/foodieswans/LoginActivity.kt
@@ -2,12 +2,29 @@ package com.PsykoloGG.foodieswans
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = Firebase.auth
     }
 
+    private fun createAccount (email: String, password: String)
+    {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                } else {
+                    Toast.makeText(baseContext, "User creation failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 
+    private lateinit var auth: FirebaseAuth
 }
